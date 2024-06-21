5d4c2a5953c0a613a9564d59c2af636187d96341
Unicorn <tang3142002@163.com>
2024-06-22 01:50:36 +0800

    注释添加

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,120 +1,210 @@
 #include "stream_reassembler.hh"
 
 #include <iostream>
-// Dummy implementation of a stream reassembler.
+// 流重组器的示例实现。
 
-// For Lab 1, please replace with a real implementation that passes the
-// automated checks run by `make check_lab1`.
+// 对于实验 1，请用一个真实的实现替换该示例实现，并通过 `make check_lab1` 运行的自动检查。
 
-// You will need to add private members to the class declaration in `stream_reassembler.hh`
+
+// 你需要在 `stream_reassembler.hh` 中的类声明中添加私有成员。
+
 
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
 
+
+
 using namespace std;
 
+
+
 StreamReassembler::StreamReassembler(const size_t capacity)
-    : _output(capacity), _capacity(capacity), _eof_index(0), _unassembled_bytes(0), _eof(false), _buf() {}
+    : _output(capacity),         // 初始化 ByteStream 对象，设置其容量为 capacity
+      _capacity(capacity),       // 设置 StreamReassembler 的容量
+      _eof_index(0),             // 初始化终止字节的索引为 0
+      _unassembled_bytes(0),     // 初始化未组装字节数为 0
+      _eof(false),               // 初始化终止标志为 false
+      _buf()                  // 初始化用于存储片段的集合为空
+{
 
-void StreamReassembler::_buf_erase(const set<Segment>::iterator &iter) {
-    _unassembled_bytes -= iter->length();
-    _buf.erase(iter);
+} 
+
+
+// 函数功能：除去已经写入流中的缓冲区的字符串
+void StreamReassembler::_buf_erase(const set<Segment>::iterator &iter) 
+{
+    _unassembled_bytes -= iter->length();  // 从未组装字节数中减去该片段的长度
+    _buf.erase(iter);                      // 从集合中移除该片段
 }
 
-void StreamReassembler::_buf_insert(const Segment &seg) {
-    _unassembled_bytes += seg.length();
-    _buf.insert(seg);
+
+// 函数功能：向缓冲区插入字符串
+void StreamReassembler::_buf_insert(const Segment &seg) 
+{
+    _unassembled_bytes += seg.length();  // 将片段的长度加到未组装字节数中
+    _buf.insert(seg);                    // 将片段插入到集合中
 }
 
-//! \details This function accepts a substring (aka a segment) of bytes,
-//! possibly out-of-order, from the logical stream, and assembles any newly
-//! contiguous substrings and writes them into the output stream in order.
-void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    if (eof) {  // 若eof==true 表明到达标志位，计算出结束下标并将标志符置为true
+
+///! \details 该函数接受来自逻辑流的一个子字符串（也称为片段），可能是无序的，
+//! 并组装任何新的连续子字符串，将它们按顺序写入输出流。
+
+// 函数功能：将子字符串 data 推入流中，将其传递给流重新组装器
+void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) 
+{
+    if (eof) 
+    {  // 若 eof 为 true，表明到达了流的末尾，计算出结束索引并将标志符置为 true
         _eof_index = index + data.size();
         _eof = true;
     }
 
-    if (!data.empty()) {
+    if (!data.empty()) 
+    {  // 如果 data 不为空，则处理该子字符串
         Segment seg{index, data};
-        _handle_substring(seg);
+        _handle_substring(seg);  // 处理该片段
     }
 
-    while (!_buf.empty() && _buf.begin()->_idx == first_unassembled()) {
+    // 将连续的片段写入输出流，并从缓冲区中移除
+    while (!_buf.empty() && _buf.begin()->_idx == first_unassembled()) 
+    {
         const auto &iter = _buf.begin();
         _output.write(iter->_data);
-        _buf_erase(iter);
+        _buf_erase(iter);  // 从缓冲区中移除已写入的片段
     }
 
-    if (_eof && first_unassembled() == _eof_index) {
+    // 如果到达了流的末尾并且所有数据都已重新组装，则结束输入并清空缓冲区
+    if (_eof && first_unassembled() == _eof_index) 
+    {
         _output.end_input();
         _buf.clear();
     }
 }
 
-void StreamReassembler::_handle_substring(Segment &seg) {
+// 函数功能：处理子字符串
+void StreamReassembler::_handle_substring(Segment &seg) 
+{
     // 共分为3种情况：
-    // 1.index越界，直接丢弃
-    // 2.部分数据越界， 裁掉一部分
+    // 1.索引越界，直接丢弃
+    // 2.部分数据越界，裁掉一部分
     // 3.都在范围内
     const size_t index = seg._idx;
 
-    // 丢弃
+    // 情况1：丢弃
     if (index >= first_unacceptable() || seg.tail_idx() < first_unassembled())
         return;
 
-    // 截断
-
-    if (index < first_unassembled() && seg.tail_idx() >= first_unassembled()) {
+    // 情况2：截断
+    // 如果片段开始索引小于第一个未组装的字节，且片段的尾部索引大于等于第一个未组装的字节
+    // 则截取片段数据，使其开始于第一个未组装的字节
+    if (index < first_unassembled() && seg.tail_idx() >= first_unassembled()) 
+    {
         seg._data = seg._data.substr(first_unassembled() - index);
         seg._idx = first_unassembled();
     }
 
+    // 如果片段的尾部索引大于第一个不可接受的字节索引，则截取片段数据，使其结束于第一个不可接受的字节之前
     if (index < first_unacceptable() && seg.tail_idx() >= first_unacceptable())
         seg._data = seg._data.substr(0, first_unacceptable() - index);
 
-    if (_buf.empty()) {
+    // 情况3：将片段插入到缓冲区中，如果缓冲区为空则直接插入
+    if (_buf.empty()) 
+    {
         _buf_insert(seg);
         return;
     }
 
+    // 处理重叠部分
     _handle_overlap(seg);
 }
 
-void StreamReassembler::_handle_overlap(Segment &seg) {
-    const size_t seg_index = seg._idx;
-    const size_t seg_tail = seg.tail_idx();
 
-    for (auto iter = _buf.begin(); iter != _buf.end();) {
-        const size_t buf_index = iter->_idx;
-        const size_t buf_tail = iter->tail_idx();
 
-        if ((seg_index <= buf_tail && seg_index >= buf_index) || (seg_tail >= buf_index && seg_index <= buf_index)) {
-            _merge_seg(seg, *iter);
-            _buf_erase(iter++);
-        } else
-            ++iter;
+// 函数功能：处理乱序、重叠的字符串
+void StreamReassembler::_handle_overlap(Segment &seg) 
+{
+    // 当前处理的片段的起始索引
+    const size_t seg_index = seg._idx;     
+
+    // 当前处理的片段的结束索引
+    const size_t seg_tail = seg.tail_idx(); 
+
+    // 遍历缓冲区中的每个片段
+    for (auto iter = _buf.begin(); iter != _buf.end();) 
+    {
+        // 缓冲区中的片段起始索引
+        const size_t buf_index = iter->_idx;    
+
+        // 缓冲区中的片段结束索引 
+        const size_t buf_tail = iter->tail_idx(); 
+
+        // 判断当前处理的片段与缓冲区中的片段是否有重叠部分
+        if ((seg_index <= buf_tail && seg_index >= buf_index) || (seg_tail >= buf_index && seg_index <= buf_index)) 
+        {
+            // 合并重叠部分，将当前处理的片段 `seg` 与缓冲区中的片段 `*iter` 合并
+            _merge_seg(seg, *iter); 
+
+            // 从缓冲区中移除已处理的片段，并将迭代器移动到下一个位置
+            _buf_erase(iter++);     
+        } 
+        else 
+        {
+            // 如果没有重叠部分，则继续检查下一个片段
+            ++iter; 
+        }
     }
 
-    _buf_insert(seg);
+    // 将当前处理的片段 `seg` 插入到缓冲区中
+    _buf_insert(seg); 
 }
 
-void StreamReassembler::_merge_seg(Segment &seg1, const Segment &seg2) {
-    // seg1是传入的字符串段，seg2是_buf中的字符串段
-    const size_t seg1_tail = seg1.tail_idx();
-    const size_t seg2_tail = seg2.tail_idx();
 
-    if (seg1._idx < seg2._idx && seg1_tail <= seg2_tail) {
-        seg1._data = seg1._data.substr(0, seg2._idx - seg1._idx) + seg2._data;
-    } else if (seg1._idx >= seg2._idx && seg1_tail > seg2_tail) {
-        seg1._data = seg2._data + seg1._data.substr(seg2._idx + seg2.length() - seg1._idx);
-        seg1._idx = seg2._idx;
-    } else if (seg1._idx >= seg2._idx && seg1_tail <= seg2_tail) {
-        seg1._data = seg2._data;
-        seg1._idx = seg2._idx;
+// 函数功能：合并字符串
+void StreamReassembler::_merge_seg(Segment &seg1, const Segment &seg2) 
+{
+    const size_t seg1_tail = seg1.tail_idx(); // seg1 的结束索引
+    const size_t seg2_tail = seg2.tail_idx(); // seg2 的结束索引
+
+    // 情况1：seg1 的起始索引小于 seg2 的起始索引，并且 seg1 的结束索引小于等于 seg2 的结束索引
+    if (seg1._idx < seg2._idx && seg1_tail <= seg2_tail) 
+    {
+        // 将 seg2 的数据插入 seg1 的对应位置
+        seg1._data = seg1._data.substr(0, seg2._idx - seg1._idx) + seg2._data; 
+    } 
+
+    // 情况2：seg1 的起始索引大于等于 seg2 的起始索引，并且 seg1 的结束索引大于 seg2 的结束索引
+    else if (seg1._idx >= seg2._idx && seg1_tail > seg2_tail) 
+    {
+        // 将 seg2 的数据拼接到 seg1 的前部分
+        seg1._data = seg2._data + seg1._data.substr(seg2._idx + seg2.length() - seg1._idx); 
+
+        // 更新 seg1 的起始索引为 seg2 的起始索引
+        seg1._idx = seg2._idx; 
+    } 
+
+    // 情况3：seg1 和 seg2 的起始索引相同，但 seg1 的结束索引小于等于 seg2 的结束索引
+    else if (seg1._idx >= seg2._idx && seg1_tail <= seg2_tail) 
+    {
+        // 直接使用 seg2 的数据替换 seg1 的数据
+        seg1._data = seg2._data; 
+
+        // 更新 seg1 的起始索引为 seg2 的起始索引
+        seg1._idx = seg2._idx; 
     }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return _unassembled_bytes; }
 
-bool StreamReassembler::empty() const { return _buf.empty(); }
+// 函数功能：返回存储但尚未重新组装的子字符串中的总字节数
+size_t StreamReassembler::unassembled_bytes() const 
+{
+    // 注意：如果特定索引处的字节被推送多次，则此函数的目的是仅计数一次。
+    return _unassembled_bytes;
+}
+
+
+// 函数功能：检查内部状态是否为空（除了输出流以外）
+bool StreamReassembler::empty() const 
+{
+    // 如果没有子字符串等待被重新组装，则返回true。
+    return _buf.empty();
+}
+
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -3,83 +3,131 @@
 
 #include "byte_stream.hh"
 
+// 这个头文件提供了一组精确宽度整数类型的 typedefs，以及一些指定整数类型限制的宏
+// 如int32_t
 #include <cstdint>
 #include <set>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
+//! \brief 一个类，将来自字节流的一系列片段（可能是无序的，可能是重叠的）组装成一个有序的字节流。
+
 class StreamReassembler {
   private:
-    struct Segment {
-        size_t _idx;
-        std::string _data;
+    struct Segment 
+    {
+        size_t _idx;        // 片段的起始索引
+        std::string _data;  // 片段包含的数据
 
+        // 构造函数
+        // 默认构造函数将 _idx 初始化为 0，将 _data 初始化为空字符串 ("")
         Segment() : _idx(0), _data() {}
+        // 参数化构造函数根据给定的 index 和 data 初始化 _idx 和 _data
         Segment(size_t index, const std::string &data) : _idx(index), _data(data) {}
 
-        size_t length() const { return _data.length(); }
-        size_t tail_idx() const { return _idx + length() - 1; }
+        // 方法
+        // 返回片段数据的长度
+        size_t length() const { return _data.length(); }   
+        // 返回片段末尾字节的索引
+        size_t tail_idx() const { return _idx + length() - 1; }  
 
+        // 运算符重载，用于基于起始索引(_idx)比较两个片段
         bool operator<(const Segment &seg) const { return this->_idx < seg._idx; }
     };
 
-  private:
-    // Your code here -- add private members as necessary.
 
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
-    size_t _eof_index;
-    size_t _unassembled_bytes;
-    bool _eof;
-    std::set<Segment> _buf;
+  private:
+    // 你的代码应在此处 - 根据需要添加私有成员。
+
+    // 重新组装的按顺序字节流
+    ByteStream _output;  
+
+    // 最大字节数
+    size_t _capacity;   
+
+    // 终止字节的索引
+    size_t _eof_index;   
+
+    // 未组装的字节数        
+    size_t _unassembled_bytes; 
+
+    // 终止标志  
+    bool _eof;        
+
+    // 使用集合存储片段数据           
+    std::set<Segment> _buf;      
+
 
     // 除去已经写入流中的缓冲区的字符串
     void _buf_erase(const std::set<Segment>::iterator &iter);
 
-    // 处理乱序、重叠的字符串
+
+    // 负责根据子字符串的索引和长度将其与当前已经接收的片段进行比较和处理
     void _handle_substring(Segment &seg);
+
+    // 处理乱序、重叠的字符串
     void _handle_overlap(Segment &seg);
+
 
     // 向缓冲区插入字符串
     void _buf_insert(const Segment &seg);
 
+
     // 合并字符串
     void _merge_seg(Segment &seg1, const Segment &seg2);
 
+
   public:
-    //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
-    //! \note This capacity limits both the bytes that have been reassembled,
-    //! and those that have not yet been reassembled.
+    //! \brief 构造一个 `StreamReassembler`，其最大存储容量为 `capacity` 字节。
+    //! \note 这个容量限制了已经重新组装的字节数
+    //! 以及尚未重新组装的字节数。
     StreamReassembler(const size_t capacity);
 
-    //! \brief Receive a substring and write any newly contiguous bytes into the stream.
+    //! \brief 接收一个子字符串并将任何新的连续字节写入流中。
     //!
-    //! The StreamReassembler will stay within the memory limits of the `capacity`.
-    //! Bytes that would exceed the capacity are silently discarded.
+    //! StreamReassembler 将保持在 `capacity` 的内存限制内。
+    //! 超过容量的字节将被静默丢弃。
     //!
-    //! \param data the substring
-    //! \param index indicates the index (place in sequence) of the first byte in `data`
-    //! \param eof the last byte of `data` will be the last byte in the entire stream
+    //! \param data 子字符串数据
+    //! \param index 表示 `data` 中第一个字节的索引（序列中的位置）
+    //! \param eof 表示 `data` 的最后一个字节将是整个流的最后一个字节
+
+
+    // 将子字符串 data 推入流中，将其传递给流重新组装器
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
 
-    //! \name Access the reassembled byte stream
-    //!@{
+
+
+    //! \name 访问重新组装的字节流
+ 
+    // 分别是用于访问 _output 的常量版本和非常量版本
     const ByteStream &stream_out() const { return _output; }
     ByteStream &stream_out() { return _output; }
-    //!@}
 
-    //! The number of bytes in the substrings stored but not yet reassembled
+
+
+    //! 存储但尚未重新组装的子字符串中的字节数量
     //!
-    //! \note If the byte at a particular index has been pushed more than once, it
-    //! should only be counted once for the purpose of this function.
+    //! \note 如果特定索引处的字节已经被推送多次，则在此函数中，仅应计数一次。
+
+    // 返回存储但尚未重新组装的子字符串中的字节数量
     size_t unassembled_bytes() const;
-    //! \brief Is the internal state empty (other than the output stream)?
-    //! \returns `true` if no substrings are waiting to be assembled
+
+
+
+
+    //! \brief 内部状态是否为空（除了输出流）？
+    //! \returns 如果没有等待组装的子字符串，则返回 `true`
     bool empty() const;
 
+    // 获取未读取的第一个字节索引
     size_t first_unread() const { return _output.bytes_read(); }
+
+    // 获取未重新组装的第一个字节索引
     size_t first_unassembled() const { return _output.bytes_written(); }
+
+    // 获取第一个不可接受的字节索引
     size_t first_unacceptable() const { return first_unread() + _capacity; }
 };
 
