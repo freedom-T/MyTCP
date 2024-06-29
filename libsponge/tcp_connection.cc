06a05f9a04e7bf5e1fd92eafbe55e011b5c419cf
Undku <undkusteam@gmail.com>
2024-06-29 19:27:27 +0800

    添加注释

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -22,29 +22,40 @@ size_t TCPConnection::time_since_last_segment_received() const { return _time_si
 
 bool TCPConnection::active() const { return _isactive; }
 
+// 从网络接收到新段时调用
 void TCPConnection::segment_received(const TCPSegment &seg) {
     if(!_isactive) return;
+    // 若连接关闭，返回
 
+    // 重设时间
     _time_since_last_segment_received = 0;
 
+    // 若连接异常，执行处理
     if(seg.header().rst){
         unclean_shutdown();
         return;
     }
 
+    // 调用receiver的received方法处理接受到的数据
     _receiver.segment_received(seg);
+
+    // 若处于监听状态，则建立连接。
     if(TCPState::state_summary(_receiver) == TCPReceiverStateSummary::SYN_RECV && 
         TCPState::state_summary(_sender) == TCPSenderStateSummary::CLOSED){
             connect();
             return;
     }
 
+    // 若为ACK不为空，则调用相应处理方法
     if(seg.header().ack)
         _sender.ack_received(seg.header().ackno, seg.header().win);
     
+    // 若接收到的数据段内容不为空，且发送队列为空，则发送个空数据段
     if(seg.length_in_sequence_space() > 0 && _sender.segments_out().empty())
         _sender.send_empty_segment();
     
+    // 调用方法，返回对应的ackno，若不为空，并且接受到的数据段不为空，
+    // 且接收到的数据顺序无误
     if(_receiver.ackno().has_value() && seg.length_in_sequence_space() == 0 &&
         seg.header().seqno == _receiver.ackno().value() - 1){
             _sender.send_empty_segment();
@@ -58,9 +69,11 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
 }
 
 size_t TCPConnection::write(const string &data) {
+    // 如果连接关闭，或要传入的数据为空，则返回
     if(!_isactive || data.empty())
         return 0;
     
+    // 发送
     size_t len = _sender.stream_in().write(data);
     _sender.fill_window();
     send_segment();
@@ -68,39 +81,48 @@ size_t TCPConnection::write(const string &data) {
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
+// 自上次调用此方法以来的毫秒数
 void TCPConnection::tick(const size_t ms_since_last_tick) {
+    // 若连接关闭，则返回
     if(!active())
         return;
     
+    // 超时重传
     _sender.tick(ms_since_last_tick);
     _time_since_last_segment_received += ms_since_last_tick;
     
+    // 若连续重传次数超过最大次数，则发送RST数据段，并————————————————————————————————————————————
     if(_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS){
         send_rst_segment();
         unclean_shutdown();
         return;
     }
 
+    // 发送数据，关掉连接
     send_segment();
     clean_shutdown();
 }
 
+// 结束输入
 void TCPConnection::end_input_stream() {
     _sender.stream_in().end_input();
     _sender.fill_window();
     send_segment();
 }
 
+// 建立连接，并填充缓冲区
 void TCPConnection::connect() {
     _sender.fill_window();
     send_segment();
 }
 
 void TCPConnection::send_segment() {
+    // 待发送队列不为空
     while(!_sender.segments_out().empty()){
         TCPSegment seg = _sender.segments_out().front();
         _sender.segments_out().pop();
 
+        // 填写头部信息
         if(_receiver.ackno().has_value()){
             seg.header().ack = true;
             seg.header().ackno = _receiver.ackno().value();
@@ -108,6 +130,7 @@ void TCPConnection::send_segment() {
                                                             _receiver.window_size() : numeric_limits<uint16_t>::max();
         }
 
+        // 进入发送队列
         _segments_out.push(seg);
     }
 }
@@ -115,6 +138,7 @@ void TCPConnection::send_segment() {
 void TCPConnection::send_rst_segment()
 {
     _sender.fill_window();
+    // 若待发送队列为空
     if(_sender.segments_out().empty()){
         _sender.send_empty_segment();
         return;
@@ -128,23 +152,32 @@ void TCPConnection::send_rst_segment()
         seg.header().win = (_receiver.window_size() <= numeric_limits<uint16_t>::max()) ? 
                                                 _receiver.window_size() : numeric_limits<uint16_t>::max();
     }
+    // 设置重连标志
     seg.header().rst = true;
 
     _segments_out.push(seg);
 }
 
+
 void TCPConnection::clean_shutdown()
 {
+    // 接收的数据全部写入输出流里的重组器
+    // 且发送端的缓存区也为空,即所有数据已发送
     if(_receiver.stream_out().input_ended() && !_sender.stream_in().eof()){
         _linger_after_streams_finish = false;
+        // 
     }
     else if(TCPState::state_summary(_sender) == TCPSenderStateSummary::FIN_ACKED && 
             TCPState::state_summary(_receiver) == TCPReceiverStateSummary::FIN_RECV){
+                // 若TCP连接处于结束且已确认状态
+                // 保留连接活跃为超时重传初始值的10倍时间，确保所有数据都成功发送且成功被接收，然后关闭连接
                 if(!_linger_after_streams_finish || _time_since_last_segment_received >= 10 * _cfg.rt_timeout)
                     _isactive = false;
+                    // 关闭连接
             }
 }
 
+// 连接异常
 void TCPConnection::unclean_shutdown()
 {
     _sender.stream_in().set_error();
@@ -152,6 +185,7 @@ void TCPConnection::unclean_shutdown()
     _isactive = false;
 }
 
+// 析构函数，回收连接
 TCPConnection::~TCPConnection() {
     try {
         if (active()) {
--- libsponge/tcp_connection.hh
+++ libsponge/tcp_connection.hh
@@ -14,80 +14,104 @@ class TCPConnection {
     TCPSender _sender{_cfg.send_capacity, _cfg.rt_timeout, _cfg.fixed_isn};
 
     //! outbound queue of segments that the TCPConnection wants sent
+    //! TCPConnection 想要发送的段的出站队列
     std::queue<TCPSegment> _segments_out{};
 
     //! Should the TCPConnection stay active (and keep ACKing)
     //! for 10 * _cfg.rt_timeout milliseconds after both streams have ended,
     //! in case the remote TCPConnection doesn't know we've received its whole stream?
+    // 在两个流都结束后，TCPConnection 应该保持活动状态（并保持 ACK）10 * _cfg.rt_timeout 毫秒，
+    // 以防远程 TCPConnection 不知道我们已经收到了它的整个流
     bool _linger_after_streams_finish{true};
 
     bool _isactive{true};
 
+    // 自上次收到数据段以来的时间
     size_t _time_since_last_segment_received{0};
 
   public:
     //! \name "Input" interface for the writer
-    //!@{
+    //!@{ 写入方的写入接口
 
     //! \brief Initiate a connection by sending a SYN segment
+    // 通过发送SYN段发起连接
     void connect();
 
     //! \brief Write data to the outbound byte stream, and send it over TCP if possible
+    // 将数据写入出站字节流，并在可能的情况下通过 TCP 发送
     //! \returns the number of bytes from `data` that were actually written.
+    // 返回实际写入的“数据”字节数。
     size_t write(const std::string &data);
 
     //! \returns the number of `bytes` that can be written right now.
+    // 现在可以写入的“字节”数。
     size_t remaining_outbound_capacity() const;
 
     //! \brief Shut down the outbound byte stream (still allows reading incoming data)
+    // 关闭出站字节流（仍然允许读取传入的数据）
     void end_input_stream();
     //!@}
 
     //! \name "Output" interface for the reader
-    //!@{
+    //!@{  读取方的读取接口
 
     //! \brief The inbound byte stream received from the peer
+    // 从对等端接收到的入站字节流
     ByteStream &inbound_stream() { return _receiver.stream_out(); }
     //!@}
 
     //! \name Accessors used for testing
+    // 用于测试的访问器
 
     //!@{
     //! \brief number of bytes sent and not yet acknowledged, counting SYN/FIN each as one byte
+    // 已发送但尚未确认的字节数，每个 SYN/FIN 算作一个字节
     size_t bytes_in_flight() const;
     //! \brief number of bytes not yet reassembled
+    // 尚未重组的字节数
     size_t unassembled_bytes() const;
     //! \brief Number of milliseconds since the last segment was received
+    // 自收到最后一个段以来的毫秒数
     size_t time_since_last_segment_received() const;
     //!< \brief summarize the state of the sender, receiver, and the connection
+    // 总结发送方、接收方和连接的状态
     TCPState state() const { return {_sender, _receiver, active(), _linger_after_streams_finish}; };
     //!@}
 
     //! \name Methods for the owner or operating system to call
-    //!@{
+    //!@{ 所有者或操作系统调用的方法
 
     //! Called when a new segment has been received from the network
+    // 从网络接收到新段时调用
     void segment_received(const TCPSegment &seg);
 
     //! Called periodically when time elapses
+    // 时间流逝时定期调用
     void tick(const size_t ms_since_last_tick);
 
     //! \brief TCPSegments that the TCPConnection has enqueued for transmission.
+    // 排队等待传输的 TCPSegments。
     //! \note The owner or operating system will dequeue these and
     //! put each one into the payload of a lower-layer datagram (usually Internet datagrams (IP),
     //! but could also be user datagrams (UDP) or any other kind).
+    // 所有者或操作系统将出队这些并将每个放入较低层数据报里（通常是互联网数据报（IP），
+    // 但也可以是用户数据报（UDP）或任何其他类型）的有效负载中。
     std::queue<TCPSegment> &segments_out() { return _segments_out; }
 
     //! \brief Is the connection still alive in any way?
+    // 连接是否仍然有效？
     //! \returns `true` if either stream is still running or if the TCPConnection is lingering
     //! after both streams have finished (e.g. to ACK retransmissions from the peer)
+    // 如果任一流仍在运行或两个流都完成后 TCPConnection 仍然存在（例如，对来自对等方的 ACK 重新传输），则为“true”
     bool active() const;
     //!@}
 
     //! Construct a new connection from a configuration
+    // 根据配置构建新连接
     explicit TCPConnection(const TCPConfig &cfg) : _cfg{cfg} {}
 
     //! \name construction and destruction
+    // 构造和析构函数
     //! moving is allowed; copying is disallowed; default construction not possible
 
     //!@{
