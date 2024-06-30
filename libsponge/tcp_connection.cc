38952c385efcdb2bcece898bb99dfd3f86566a39
Undku <undkusteam@gmail.com>
2024-06-30 12:37:55 +0800

    添加注释

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -30,13 +30,13 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
     // 重设时间
     _time_since_last_segment_received = 0;
 
-    // 若连接异常，执行处理
+    // 如果RST（reset）标志位为真，将发送端stream和接受端stream设置成error state并终止连接。
     if(seg.header().rst){
         unclean_shutdown();
         return;
     }
 
-    // 调用receiver的received方法处理接受到的数据
+    // 把segment传递给TCPReceiver，这样的话，TCPReceiver就能从segment取出它所关心的字段进行处理了：seqno，SYN，payload，FIN。
     _receiver.segment_received(seg);
 
     // 若处于监听状态，则建立连接。
@@ -46,16 +46,15 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
             return;
     }
 
-    // 若为ACK不为空，则调用相应处理方法
+    // 如果ACK标志位为真，通知TCPSender有segment被确认，TCPSender关心的字段有ackno和window_size
     if(seg.header().ack)
         _sender.ack_received(seg.header().ackno, seg.header().win);
     
-    // 若接收到的数据段内容不为空，且发送队列为空，则发送个空数据段
+    // 如果收到的segment不为空，TCPConnection必须确保至少给这个segment回复一个ACK，以便远端的发送方更新ackno和window_size
     if(seg.length_in_sequence_space() > 0 && _sender.segments_out().empty())
         _sender.send_empty_segment();
     
-    // 调用方法，返回对应的ackno，若不为空，并且接受到的数据段不为空，
-    // 且接收到的数据顺序无误
+    // 此为空数据段，目的为测试连接是否有效
     if(_receiver.ackno().has_value() && seg.length_in_sequence_space() == 0 &&
         seg.header().seqno == _receiver.ackno().value() - 1){
             _sender.send_empty_segment();
@@ -91,7 +90,7 @@ void TCPConnection::tick(const size_t ms_since_last_tick) {
     _sender.tick(ms_since_last_tick);
     _time_since_last_segment_received += ms_since_last_tick;
     
-    // 若连续重传次数超过最大次数，则发送RST数据段，并————————————————————————————————————————————
+    // 若连续重传次数超过最大次数，则发送RST数据段，并关闭连接
     if(_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS){
         send_rst_segment();
         unclean_shutdown();
@@ -162,10 +161,10 @@ void TCPConnection::send_rst_segment()
 void TCPConnection::clean_shutdown()
 {
     // 接收的数据全部写入输出流里的重组器
-    // 且发送端的缓存区也为空,即所有数据已发送
+    // 但发送端的缓存区不为空
     if(_receiver.stream_out().input_ended() && !_sender.stream_in().eof()){
         _linger_after_streams_finish = false;
-        // 
+        // 关闭receiver和sender的stream流结束之后的等待，即linger状态。
     }
     else if(TCPState::state_summary(_sender) == TCPSenderStateSummary::FIN_ACKED && 
             TCPState::state_summary(_receiver) == TCPReceiverStateSummary::FIN_RECV){
